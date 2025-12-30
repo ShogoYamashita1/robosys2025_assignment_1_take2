02d94c1aae42f601f4c53c7a86fd8adef9e0a8e3
Shogo Yamashita <s24c1130pf@s.chibakoudai.jp>
2025-12-30 23:04:02 +0900

    コマンドのフラグ処理をswitchでまとめ直した

--- dice_roll.c
+++ dice_roll.c
@@ -14,55 +14,54 @@ int GetRandom(int min, int max)
 
 
 int main(void){
-int input, dice_count = 0, face_count = 0, take = 0;
-static int flag;
+int input, dice_count = 0, face_count = 0, flag = 0;
 
-// 入力確認と数字分け
+// 入力処理
 while((input = getchar()) != EOF){
-    if(isalpha(input)){// 文字
-        if( (input == 'd' || input == 'D') && flag != 2 && take != 0 ){
-            flag = 1;
-            take = 0;
-        }
-        else{flag = 2;}
-    }
-    else if(isdigit(input)){// 数字
-        if(flag == 0){
-            if( !(take == 0 && input == '0') ){
-                dice_count *= 10;
-                dice_count += (input - '0');
-                take++;
-            }
-            else{
-                flag = 2;
-            }
-        }
-        else if(flag != 2){
-            if( !(take == 0 && input == '0') ){
-                face_count *= 10;
-                face_count += (input - '0');
-                take++;
-                flag = 3;
-            }
-            else{flag = 2;}
+    if(isdigit(input)){// 数値入力判定
+        switch(flag){
+            case 0:if(input == '0'){flag = -1;break;}// 0始まりならエラー
+            case 1:if(flag != -1){// ロール数判定処理
+                        flag = 1;
+                        dice_count *= 10;
+                        dice_count += (input - '0');
+                   }break;
+
+            case 2:if(input == '0'){flag = -1;break;}// 0始まりならエラー
+            case 3:if(flag != -1){// 面数判定処理
+                        flag = 3;
+                        face_count *= 10;
+                        face_count += (input - '0');
+                   }break;
+
+            default:break;
         }
     }
-    else if(input != '\n'){
-        flag = 2;
+
+    else if(isalpha(input)){// 文字入力判定.dまたはDの時のみパス
+        if( (input == 'd' || input == 'D') && flag == 1 ){
+            flag = 2;
+        }
+        else{flag = -1;}
+    }
+
+    else if(input != '\n'){// その他の異常入力判定
+        flag = -1;
     }
 }
+
 
 
 // 終了処理
-if(flag == 3){
+if(flag == 3){// 正常終了
     srand((unsigned int)time(NULL));
 
     for(int i = 0; i < dice_count; i++){
-        printf("%d\n", GetRandom(1, face_count));
+        printf("%d\n", GetRandom(1, face_count));// ここでダイスロール
     }
     return 0;
 }
-else{
+else{// 異常終了
     fprintf(stderr, "\x1b[31merror: An invalid value was entered\x1b[0m\n");
     return 1;
 }
